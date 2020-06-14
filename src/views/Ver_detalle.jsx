cff905d0b2be19978eac176396ff7b5bf52accbd
MarcoClavero <marco.clavero@mail.udp.cl>
2020-06-14 19:18:10 -0400

    Updatev2

--- src/views/Ver_detalle.jsx
+++ src/views/Ver_detalle.jsx
@@ -30,6 +30,7 @@ class Ver_detalle extends Component {
                       ncols={["col-md-5", "col-md-7"]}
                       properties={[
                         {
+                          //disabled: "disabled",
                           label: "RUT",
                           type: "text",
                           bsClass: "form-control",
