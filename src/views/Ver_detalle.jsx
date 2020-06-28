27b2dbda8ff6451ffadc2112afbffc3510e2c8fc
nicolas-henriquez <nicolashmunoz@gmail.com>
2020-06-27 21:10:30 -0400

    Modificar detalle alumno

--- src/views/Ver_detalle.jsx
+++ src/views/Ver_detalle.jsx
@@ -165,7 +165,7 @@ class Ver_detalle extends Component {
                     <Button bsStyle="info" pullLeft fill onClick={this.openRegistro.bind(this)}>
                       Ver Registro
                     </Button>
-                    <Button bsStyle="info" pullRight fill type="submit" href="Modificar_detalle">
+                    <Button bsStyle="info" pullRight fill type="submit" href={`http://localhost:3000/admin/Modificar_detalle/${this.state.alumno.rut}`}>
                       Modificar datos
                     </Button>
                     <div className="clearfix" />
